ef48ec90cbbf3448dae8f9a07c6c9a2b02627727
GovindPatil75 <mulegr75@gmail.com>
2022-03-12 16:16:37 +0530

    tc 003

---
+++ src/test/java/com/TDDFramework/Test/test_003.java
@@ -0,0 +1,11 @@
+package com.TDDFramework.Test;
+
+public class test_003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test 3");
+	}
+
+}
